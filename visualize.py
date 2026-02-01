a7d55165f71598523b3acf57c7cd557cdc56885d
Michal Mrowczyk <michal3141@op.pl>
2026-02-01 22:44:28 +0100

    Fixing some minor issues, adding more visualizations and adding analysis for collisions with homes

--- visualize.py
+++ visualize.py
@@ -25,24 +25,56 @@ def visualize(criterion):
             arr2 = variant.strip().split('W')
             z.append(int(arr2[1]))
             i += 1
-    plt.scatter(x, y)
+    plt.scatter(x, y, c=y)
     plt.xlabel('Pozycja wariantu w rankingu')
     plt.ylabel('Numer węzła początkowego')
     plt.title('WAP węzły %s' % criterion)
     plt.show()
 
-    plt.scatter(x, z)
+    plt.scatter(x, z, c=y)
     plt.xlabel('Pozycja wariantu w rankingu')
     plt.ylabel('Pierwszy odcinek decyzyjny')
     plt.title('WAP odcinki %s' % criterion)
     plt.show()
 
+def compare_waps():
+    variant_to_index = {}
+    with open('WAP_Ranking.csv', 'r') as f:
+        f.readline()
+        for line in f:
+            arr = line.strip().split(',')
+            index = int(arr[0])
+            variant = '%s%s%s' % (arr[3], arr[4], arr[5])
+            variant_to_index[variant] = index
+    i = 1
+    x = []
+    y = []
+    c = []
+    with open('sumarycznie.csv', 'r') as f:
+        for line in f:
+            arr = line.strip().split(',')
+            origin  = arr[0]
+            c.append(origin_to_index[origin])
+            variant = arr[1]
+            x.append(i)
+            y.append(variant_to_index[variant])
+            i += 1
+    plt.scatter(x, y, c=c)
+    plt.xlabel('Pozycja wariantu w rankingu WAP')
+    plt.ylabel('Pozycja wariantu w rankingu IVIA')
+    plt.title('WAP vs IVIA')
+    plt.show()
+  
+
+
 def main():
+    compare_waps()
     visualize('sumarycznie')
     visualize('przestrzenno-spoleczne')
     visualize('ekonomiczne')
     visualize('srodowiskowe')
     visualize('transportowe')
+    visualize('budynki-mieszkalne')
 	
 
 if __name__ == '__main__':
